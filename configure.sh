d10e4225d00324595ee0033957403fd3f59abb44
ZhaoXiaofei <cndfeifei@hotmail.com>
2023-12-09 01:37:16 +0800

    Fixed a typo with varname LONG. Commented-out NEORANKING_RESOURCE because we usually export it via the cmd-line.

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 # configure paths before running python scripts
 
-export NEORANKING_RESOURCE=/mnt/d/code/NeoRanking_resource #"/home/localadmin/Priorization/paper"
-export NEORANKING_CODE=/mnt/d/code/NeoRanking #"/home/localadmin/Priorization/paper_code"
+#export NEORANKING_RESOURCE=/mnt/d/code/NeoRanking_resource/isotonic1b #"/home/localadmin/Priorization/paper"
+export NEORANKING_CODE=/mnt/d/code/NeoRanking/isotonic1 #"/home/localadmin/Priorization/paper_code"
 
 mkdir -p "$NEORANKING_RESOURCE/data"
 mkdir -p "$NEORANKING_RESOURCE/plots"
@@ -11,5 +11,5 @@ mkdir -p "$NEORANKING_RESOURCE/data/hla"
 mkdir -p "$NEORANKING_RESOURCE/data/cat_encoding"
 
 SHORT=neopep
-LOGN=mutation
+LONG=mutation
 
