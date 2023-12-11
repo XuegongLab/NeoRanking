838d6037e98056e6911bbaebd9e59fa7952635da
ZhaoXiaofei <cndfeifei@hotmail.com>
2023-12-11 14:46:02 +0800

    In plot_figure_4.sh: changed long into ${LONG}.

--- plot_figure_4.sh
+++ plot_figure_4.sh
@@ -1,10 +1,10 @@
 source configure.sh
 
-CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotClassifierResults.py -fn Figure_4A -re LR_*_long_*test.txt XGBoost_*_long_*test.txt Voting_classifier_0.50_long_test.txt -pt mutation -g \"{'TESLA':'TESLA','HiTIDE':'HiTIDE','NCI_test':'NCI-test','NCI_train':'NCI-train'}\" -cln \"{0:'LR',1:'XGBoost',2:'Voting'}\" -a 0.02 -rot 30 -las 30 -tis 18 -les 15 -sr -ga -o \"Gartner et al.,LR,XGBoost,Voting\" -ds NCI_test -plt topn_counts"
+CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotClassifierResults.py -fn Figure_4A -re LR_*_${LONG}_*test.txt XGBoost_*_${LONG}_*test.txt Voting_classifier_0.50_${LONG}_test.txt -pt mutation -g \"{'TESLA':'TESLA','HiTIDE':'HiTIDE','NCI_test':'NCI-test','NCI_train':'NCI-train'}\" -cln \"{0:'LR',1:'XGBoost',2:'Voting'}\" -a 0.02 -rot 30 -las 30 -tis 18 -les 15 -sr -ga -o \"Gartner et al.,LR,XGBoost,Voting\" -ds NCI_test -plt topn_counts"
 echo $CMD
 eval $CMD
 
-CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotShapleyImportance.py -c1 LR_*_long_*.sav -c2 XGBoost_*_long_*.xgbm -ds1 NCI_train -pt mutation -ft pdf -fn Figure_4B -las 30 -tis 23 -tts 25 -les 20 -cm \"{'LR':0,'XGBoost':3}\""
+CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotShapleyImportance.py -c1 LR_*_${LONG}_*.sav -c2 XGBoost_*_${LONG}_*.xgbm -ds1 NCI_train -pt mutation -ft pdf -fn Figure_4B -las 30 -tis 23 -tts 25 -les 20 -cm \"{'LR':0,'XGBoost':3}\""
 echo $CMD
 eval $CMD
 
