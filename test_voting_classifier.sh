1f48886cff44a77c85f738312b2a99278137d4e2
ZhaoXiaofei <cndfeifei@hotmail.com>
2023-12-11 14:59:34 +0800

    In test_voting_classifier.sh: changed _short_ into _${SHORT}_ and _long_ into _${LONLONG}_.

--- test_voting_classifier.sh
+++ test_voting_classifier.sh
@@ -2,11 +2,11 @@
 
 source configure.sh
 
-cmd="PYTHONPATH=$NEORANKING_CODE python3 Classifier/TestVotingClassifier.py -c1 LR_*_${SHORT}_*.sav -c2 XGBoost_*_short_*.xgbm -w 0.5 -tr NCI_train -te NCI_test -te TESLA -te HiTIDE -pt ${SHORT}"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Classifier/TestVotingClassifier.py -c1 LR_*_${SHORT}_*.sav -c2 XGBoost_*_${SHORT}_*.xgbm -w 0.5 -tr NCI_train -te NCI_test -te TESLA -te HiTIDE -pt ${SHORT}"
 echo $cmd
 eval $cmd
 
-cmd="PYTHONPATH=$NEORANKING_CODE python3 Classifier/TestVotingClassifier.py -c1 LR_*_${LONG}_*.sav -c2 XGBoost_*_long_*.xgbm -w 0.5 -tr NCI_train -te NCI_test -te TESLA -te HiTIDE -pt ${LONG}"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Classifier/TestVotingClassifier.py -c1 LR_*_${LONG}_*.sav -c2 XGBoost_*_${LONG}_*.xgbm -w 0.5 -tr NCI_train -te NCI_test -te TESLA -te HiTIDE -pt ${LONG}"
 echo $cmd
 eval $cmd
 
