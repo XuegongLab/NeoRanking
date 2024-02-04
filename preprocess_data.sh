3f8a1e01f66510c3a71294243fdbf5a6f0b41ed6
ZhaoXiaofei <cndfeifei@hotmail.com>
2024-02-04 17:54:48 +0800

    Let preprocess_data.sh be able to use category encoder and feature transformer separately instead of using one class for both.

--- preprocess_data.sh
+++ preprocess_data.sh
@@ -1,18 +1,19 @@
 source configure.sh
 
-isopath="${1}"
-seed="${2}"
+isopath1="${1}"
+isopath2="${2}"
+seed="${3}"
 
 # select rows used for ML. These are all somatic SNV mutations.
 PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/SelectMLData.py -pt neopep
 PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/SelectMLData.py -pt mutation
 
 # calculate numerical encoding for categorical values based on NCI_train
-PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/CalcCatEncodings.py -pt neopep -ds NCI_train
-PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/CalcCatEncodings.py -pt mutation -ds NCI_train
+PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/CalcCatEncodings.py -pt neopep -ds NCI_train   --isopath "${isopath1}"
+PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/CalcCatEncodings.py -pt mutation -ds NCI_train --isopath "${isopath1}"
 
 # normalize (missing value imputation, data normalization, and replacing categories by their encoded numerical values
-PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt neopep -ds NCI_train -o ml     --isopath "${isopath}" --seed "${seed}"
-PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt mutation -ds NCI_train -o ml   --isopath "${isopath}" --seed "${seed}"
-PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt neopep -ds NCI_train -o plot   --isopath "${isopath}" --seed "${seed}"
-PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt mutation -ds NCI_train -o plot --isopath "${isopath}" --seed "${seed}"
+PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt neopep -ds NCI_train -o ml     --isopath "${isopath2}" --seed "${seed}"
+PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt mutation -ds NCI_train -o ml   --isopath "${isopath2}" --seed "${seed}"
+PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt neopep -ds NCI_train -o plot   --isopath "${isopath2}" --seed "${seed}"
+PYTHONPATH=$NEORANKING_CODE python3 DataWrangling/NormalizeData.py -pt mutation -ds NCI_train -o plot --isopath "${isopath2}" --seed "${seed}"
